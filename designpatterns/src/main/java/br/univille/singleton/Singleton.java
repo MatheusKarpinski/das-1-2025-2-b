08ccffd9a55677e45e99a146069b109def795a66
MatheusKarpinski <karpinskimatheus@gmail.com>
2025-09-18 22:44:32 +0000

    feat: arrumei o designpatterns

--- designpatterns/src/main/java/br/univille/singleton/Singleton.java
+++ designpatterns/src/main/java/br/univille/singleton/Singleton.java
@@ -1,15 +1,25 @@
 package br.univille.singleton;
 
 public class Singleton {
-    private static Singleton instance; //parece com uma variavel global
+    //parace com uma variável global
+    private static Singleton instance;
+    private String segredo;
 
+    public String getSegredo() {
+        return segredo;
+    }
+    public void setSegredo(String segredo) {
+        this.segredo = segredo;
+    }
     private Singleton(){
-
-    } 
+        System.out.println("Singleton: Construtor");
+    }
     public static Singleton getInstance(){
-        if(instance == null){
+        System.out.println("Singleton: getInstance");
+        if (instance == null){
+            System.out.println("Singleton: NEW");
             instance = new Singleton();
         }
         return instance;
-    }  
+    }
 }
